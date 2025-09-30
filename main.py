b52a96445c3708318afa7bdc8ca96c4413ec7def
afrank4635 <andrew.tc.frank@gmail.com>
2025-09-30 12:49:43 +0900

    Add and initialize Player

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import pygame
-from constants import *  #imports everything, generallly not a good idea but okay for small projects
 
+# run "source .venv/bin/activate" in command line first
+
+import pygame
+from constants import *  # imports everything, generallly not a good idea but okay for small projects
+from player import Player 
 
 def main():
     pygame.init()
@@ -9,7 +12,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0.0   
+    dt = 0.0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     
     while True:
@@ -17,7 +22,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-        screen.fill(("black"))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         # limit to 60 FPS
